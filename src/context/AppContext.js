0e0c235249ff061399dc20d7f5dc8e382f5645a5
Samuel okoye <samuelokoye92@gmail.com>
2024-04-13 21:44:09 +0200

    mdf

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,13 @@
 import React, { createContext, useReducer } from "react";
 
-const initailState = {
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const initialState = {
   budget: 2000,
   expenses: [
     { id: 12, name: "shopping", cost: 40 },
@@ -11,8 +18,53 @@ const initailState = {
 export const AppContext = createContext();
 
 const AppProvider = (props) => {
-  const [state, dispatch] = useReducer();
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 };
 
-const chemists = people.filter((person) => person.profession === "chemist");
-const listItems = chemists.map((person) => <li></li>);
+
+
+
+
+
+function Testing() {
+  const [profile, setProfile] = useSate({
+    name: Oluwaye Olayinbi,
+    carrier: {
+      Teaching: Doctorate in physics,
+      Sports: football,
+      ministry: pastor
+    }
+  });
+
+  function handleSubmit() {
+    setProfile({
+      ...profile,
+      name: e.target.value
+    });
+  }
+
+  function handleTeachingChange(e) {
+    setProfile({
+      ...profile,
+      carrier: {
+        ...profile.carrier,
+        Sports: e.target.value
+      }
+    })
+      
+  }
+
+  return (
+    <>
+      <form Onsubmit={handleSubmit}>
+        <textarea placeholder='Message' value={message} />
+        <button type='Submit'>
+          click
+        </button>
+      </form>
+    </>
+  )
+}
+
+
+
