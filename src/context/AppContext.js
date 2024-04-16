8a5006a723a863e8d1217b93112e796cf2d61218
Samuel okoye <samuelokoye92@gmail.com>
2024-04-16 22:36:38 +0200

    feat-reducer

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,7 +5,7 @@ const AppReducer = (state, action) => {
     default:
       return state;
   }
-}
+};
 
 const initialState = {
   budget: 2000,
@@ -17,54 +17,17 @@ const initialState = {
 
 export const AppContext = createContext();
 
-const AppProvider = (props) => {
+export const AppProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-};
-
-
-
-
-
-
-function Testing() {
-  const [profile, setProfile] = useSate({
-    name: Oluwaye Olayinbi,
-    carrier: {
-      Teaching: Doctorate in physics,
-      Sports: football,
-      ministry: pastor
-    }
-  });
-
-  function handleSubmit() {
-    setProfile({
-      ...profile,
-      name: e.target.value
-    });
-  }
-
-  function handleTeachingChange(e) {
-    setProfile({
-      ...profile,
-      carrier: {
-        ...profile.carrier,
-        Sports: e.target.value
-      }
-    })
-      
-  }
-
   return (
-    <>
-      <form Onsubmit={handleSubmit}>
-        <textarea placeholder='Message' value={message} />
-        <button type='Submit'>
-          click
-        </button>
-      </form>
-    </>
-  )
-}
-
-
-
+    <AppContext.Provider
+      value={{
+        budget: state.budget,
+        expenses: state.expense,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
+};
